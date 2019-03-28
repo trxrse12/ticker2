685474b6c46a2d0a5bcc835ecffaf4dbd34e27cc
trxrse12 <trxrse@gmail.com>
2019-03-28 18:03:09 +0000

    The app is now validating the received JSON against the predefined schema

--- src/validators/stocks/movers/index.js
+++ src/validators/stocks/movers/index.js
@@ -1,11 +1,15 @@
 const Ajv = require('ajv');
-const tickerAnswerSchema = require('../../../schema/stocks/ticker2_answer');
+
+const tickerAnswerSchema = require('../../../schema/stocks/ticker2-answer');
+const tickerStockClassSchema = require('../../../schema/stocks/ticker2-stock-class');
+const tickerStockItem = require('../../../schema/stocks/ticker2-stock-class');
+
 const ValidationError = require('../../errors/validation-error');
 const generateValidationErrorMessage = require('../../errors/messages');
 
 function validate(req) {
   const ajvValidate = new Ajv()
-    .addSchema([tickerAnswerSchema])
+    .addSchema([tickerAnswerSchema, tickerStockClassSchema])
     .compile(tickerAnswerSchema);
   const valid = ajvValidate(req.body);
   if (!valid) {
